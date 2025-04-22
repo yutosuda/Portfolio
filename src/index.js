c6bfec879e9c58b24bb91b4a69bbb141d168424d
_aruday <yuto.suda1024@gmail.com>
2025-04-22 10:50:26 +0900

    navができた
    Update navigation links and enhance styling for improved user experience
    
    - Replaced existing navigation links in the `Overlay` component with new links to external resources, including GitHub, Twitter, and various note articles.
    - Improved the hover effect for links in `styles.css` by applying `!important` to ensure visibility.
    - Added a new style rule for `.menu-item a` to maintain consistent link colors across the menu items.
    
    These changes aim to streamline user navigation and enhance the overall functionality of the application.

--- src/index.js
+++ src/index.js
@@ -37,209 +37,48 @@ function Overlay() {
         </h1>
         <ul style={{ listStyle: 'none', padding: 0, margin: 0 }}>
           <li className="menu-item">
-            <a href="/crystallization" style={{ color: '#999999', textDecoration: 'none' }}>
-              CRYSTALLIZATION
+            <a href="https://github.com/yutosuda" target="_blank" style={{ color: '#999999', textDecoration: 'none' }}>
+              Github
             </a>
           </li>
           <li className="menu-item">
-            <a href="/disco" style={{ color: '#999999', textDecoration: 'none' }}>
-              DISCO
+            <a href="https://twitter.com/_aruday" target="_blank" style={{ color: '#999999', textDecoration: 'none' }}>
+              X（Twitter）
             </a>
           </li>
           <li className="menu-item">
-            <a href="/jellyfish2" style={{ color: '#999999', textDecoration: 'none' }}>
-              JELLY FISH 2
+            <a href="https://note.com/aruday" target="_blank" style={{ color: '#999999', textDecoration: 'none' }}>
+              note
             </a>
           </li>
           <li className="menu-item">
-            <a href="/malachite" style={{ color: '#999999', textDecoration: 'none' }}>
-              MALACHITE
+            <a href="https://note.com/aruday/n/n0bafd0b58211" style={{ color: '#999999', textDecoration: 'none' }}>
+              『スイカ入門』 初心者編
             </a>
           </li>
           <li className="menu-item">
-            <a href="/brush" style={{ color: '#999999', textDecoration: 'none' }}>
-              BRUSH
+            <a href="https://note.com/aruday/n/n01a981656686" style={{ color: '#999999', textDecoration: 'none' }}>
+              『スイカ入門』 意思決定編
             </a>
           </li>
           <li className="menu-item">
-            <a href="/threerects" style={{ color: '#999999', textDecoration: 'none' }}>
-              THREE RECTS
+            <a href="https://note.com/aruday/n/n25596294368e" style={{ color: '#999999', textDecoration: 'none' }}>
+              『スイカ入門』 ホモ・サピエンス編
             </a>
           </li>
           <li className="menu-item">
-            <a href="/nightmare2" style={{ color: '#999999', textDecoration: 'none' }}>
-              NIGHTMARE 2
+            <a href="https://note.com/aruday/n/n21b68ab90a27" style={{ color: '#999999', textDecoration: 'none' }}>
+              『一入門』 起業・経営編
             </a>
           </li>
           <li className="menu-item">
-            <a href="/jellyfish" style={{ color: '#999999', textDecoration: 'none' }}>
-              JELLY FISH
+            <a href="https://note.com/aruday/n/n21fba33f3fa7" style={{ color: '#999999', textDecoration: 'none' }}>
+              『ラクダ入門』営業・事業編
             </a>
           </li>
           <li className="menu-item">
-            <a href="/watercolor" style={{ color: '#999999', textDecoration: 'none' }}>
-              WATER COLOR
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/nerves" style={{ color: '#999999', textDecoration: 'none' }}>
-              NERVES
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/drylandscape" style={{ color: '#999999', textDecoration: 'none' }}>
-              DRY LANDSCAPE
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/photodecay" style={{ color: '#999999', textDecoration: 'none' }}>
-              PHOTO DECAY
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/nightmare" style={{ color: '#999999', textDecoration: 'none' }}>
-              NIGHTMARE
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/dotparade" style={{ color: '#999999', textDecoration: 'none' }}>
-              DOT PARADE
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/specimenofpixels" style={{ color: '#999999', textDecoration: 'none' }}>
-              SPECIMEN OF PIXELS
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/flow" style={{ color: '#999999', textDecoration: 'none' }}>
-              FLOW
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/pixelacid" style={{ color: '#999999', textDecoration: 'none' }}>
-              PIXEL ACID
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/specimenofmotions" style={{ color: '#999999', textDecoration: 'none' }}>
-              SPECIMEN OF MOTIONS
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/voronoi" style={{ color: '#999999', textDecoration: 'none' }}>
-              VORONOI
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/growth" style={{ color: '#999999', textDecoration: 'none' }}>
-              GROWTH
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/proliferate" style={{ color: '#999999', textDecoration: 'none' }}>
-              PROLIFERATE
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/mesh" style={{ color: '#999999', textDecoration: 'none' }}>
-              MESH
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/stratum" style={{ color: '#999999', textDecoration: 'none' }}>
-              STRATUM
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/kaleidoscope" style={{ color: '#999999', textDecoration: 'none' }}>
-              KALEIDOSCOPE
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/tidyup" style={{ color: '#999999', textDecoration: 'none' }}>
-              TIDY UP
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/marathonman" style={{ color: '#999999', textDecoration: 'none' }}>
-              MARATHON MAN
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/fillpill2" style={{ color: '#999999', textDecoration: 'none' }}>
-              FILL PILL2
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/fillpill" style={{ color: '#999999', textDecoration: 'none' }}>
-              FILL PILL
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/supernova" style={{ color: '#999999', textDecoration: 'none' }}>
-              SUPER NOVA
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/moire" style={{ color: '#999999', textDecoration: 'none' }}>
-              MOIRE
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/sakao" style={{ color: '#999999', textDecoration: 'none' }}>
-              SAKAO
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/feedme" style={{ color: '#999999', textDecoration: 'none' }}>
-              FEED ME
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/spin" style={{ color: '#999999', textDecoration: 'none' }}>
-              SPIN
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/springzoo" style={{ color: '#999999', textDecoration: 'none' }}>
-              SPRING ZOO
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/amoeba" style={{ color: '#999999', textDecoration: 'none' }}>
-              AMOEBA
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/wormpainting" style={{ color: '#999999', textDecoration: 'none' }}>
-              WORM PAINTING
-            </a>
-          </li>
-          <li style={{ marginBottom: '20px' }}></li>
-          <li className="menu-item">
-            <a href="/blog" style={{ color: '#999999', textDecoration: 'none' }}>
-              BLOG
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="/works" style={{ color: '#999999', textDecoration: 'none' }}>
-              WORKS
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="https://twitter.com/flapdict" target="_blank" style={{ color: '#999999', textDecoration: 'none' }}>
-              TWITTER
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="https://www.facebook.com/flapdict" target="_blank" style={{ color: '#999999', textDecoration: 'none' }}>
-              FACEBOOK
-            </a>
-          </li>
-          <li className="menu-item">
-            <a href="mailto:info@flapdict.com" style={{ color: '#999999', textDecoration: 'none' }}>
-              MAIL
+            <a href="https://note.com/aruday/n/ncdd8b17e1ff1" style={{ color: '#999999', textDecoration: 'none' }}>
+              『安藤入門』ファイナンス編
             </a>
           </li>
         </ul>
